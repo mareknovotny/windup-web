5cb5287eb43c5db61a508cf9344edd36116577e6
Marek Novotný <hotmana76@gmail.com>
2017-06-15 15:13:13 +0200

    WINDUP-1432: release script enhancements

--- build/release_windup_web.sh
+++ build/release_windup_web.sh
@@ -16,7 +16,7 @@ function release_windup {
 
         cd $REPODIR
         echo Releasing \"$REPO\" - $REL \(Next dev version is $DEV\)
-        mvn release:prepare-with-pom clean install \
+        mvn release:prepare-with-pom \
                 -DdevelopmentVersion=$DEV \
                 -DreleaseVersion=$REL \
                 -Dtag=$REL \
@@ -24,13 +24,15 @@ function release_windup {
                 -Darguments=-DskipTests \
                 -Dmvn.test.skip=true \
                 -Dfurnace.dot.skip \
-                -Dwebpack.environment=production
+                -Dwebpack.environment=production \
+                -DpreparationGoals=clean,install \
+                -DpushChanges=false || return 1;
 
         echo "Priming build for $REPO"
-        mvn -DskipTests clean install -Dwebpack.environment=production
+        mvn -DskipTests  -Dwebpack.environment=production 
 
         echo "Prepare build for $REPO"
-        mvn release:prepare clean install \
+        mvn release:prepare \
                 -DdevelopmentVersion=$DEV \
                 -DreleaseVersion=$REL \
                 -Dtag=$REL \
@@ -38,7 +40,9 @@ function release_windup {
                 -Darguments=-DskipTests \
                 -Dmvn.test.skip=true \
                 -Dfurnace.dot.skip \
-                -Dwebpack.environment=production
+                -Dwebpack.environment=production \
+                -DpreparationGoals=clean,install \
+                -DpushChanges=false || return 1;
 
         echo "Finished preparing release"
 
@@ -50,7 +54,9 @@ function release_windup {
                 -Darguments=-DskipTests \
                 -Dmvn.test.skip=true \
                 -Dfurnace.dot.skip \
-                -Dwebpack.environment=production
+                -Dwebpack.environment=production \
+                -DpushChanges=false || return 1;
+
         cd ..
 }
 
@@ -64,43 +70,55 @@ git clone git@github.com:windup/windup-keycloak-tool.git
 git clone git@github.com:windup/windup-web.git
 git clone git@github.com:windup/windup-web-distribution.git
 
+
 cd windup-web
 sed -i -e "s/<version.windup>.*<\/version.windup>/<version.windup>$REL<\/version.windup>/g" pom.xml
+
 cd tsmodelsgen-invocation
 sed -i -e "s/<version.windup.core>.*<\/version.windup.core>/<version.windup.core>$REL<\/version.windup.core>/g" pom.xml
-sed -i -e "s/<version.windup.web>.*<\/version.windup.web>/<version.windup.web>$REL<\/version.windup.web>/g" pom.xml
+
 cd ../tsmodelsgen-maven-plugin
 sed -i -e "s/<version.windup.core>.*<\/version.windup.core>/<version.windup.core>$REL<\/version.windup.core>/g" pom.xml
 sed -i -e "s/<version.windup.web>.*<\/version.windup.web>/<version.windup.web>$REL<\/version.windup.web>/g" pom.xml
 
-
 cd ..
 
-git add -A
-git commit -a -m "Preparing for release"
-git push origin
+git add -u && git commit -a -m "Preparing for release $REL"
+# push all manually at the end after checking everything is ok
+#git push origin
+
 cd ../
 
+# this is not required to be done if there is not any change from previous release
 release_windup git@github.com:windup/windup-keycloak-tool.git windup-keycloak-tool
+
+# MAIN release block
 release_windup git@github.com:windup/windup-web.git windup-web
 release_windup git@github.com:windup/windup-web-distribution.git windup-web-distribution
 
 cd windup-web
 sed -i -e "s/<version.windup>.*<\/version.windup>/<version.windup>$DEV<\/version.windup>/g" pom.xml
+
 cd tsmodelsgen-invocation
 sed -i -e "s/<version.windup.core>.*<\/version.windup.core>/<version.windup.core>$DEV<\/version.windup.core>/g" pom.xml
-sed -i -e "s/<version.windup.web>.*<\/version.windup.web>/<version.windup.web>$DEV<\/version.windup.web>/g" pom.xml
+
 cd ../tsmodelsgen-maven-plugin
 sed -i -e "s/<version.windup.core>.*<\/version.windup.core>/<version.windup.core>$DEV<\/version.windup.core>/g" pom.xml
 sed -i -e "s/<version.windup.web>.*<\/version.windup.web>/<version.windup.web>$DEV<\/version.windup.web>/g" pom.xml
 cd ..
-git add -A
-git commit -a -m "Back to development"
-git push origin
+
+git add -u && git commit -a -m "Back to development"
+# push all manually at the end after checking everything is ok
+#git push origin
+
 cd ../
 
 #
 # #open https://repository.jboss.org/nexus/index.html
+# check the builds:
+#   * like running Web Console with some applications
+#   * deploy to openshift/minishift
+
 # #echo "Cleaning up temp directory $WORK_DIR"
 # echo "Done"
 # #rm -rf $WORK_DIR
